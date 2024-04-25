8bce895e97dbf775673dffcca130693f6b0cf628
Gargi <pallavi.gaikwad0399@gmail.com>
2024-04-25 16:54:57 +0530

    Commit

--- src/main/java/RandomNumber/Approach3.java
+++ src/main/java/RandomNumber/Approach3.java
@@ -1,31 +1,24 @@
 package RandomNumber;
 import java.util.Random;
-
+import java.util.concurrent.ThreadLocalRandom;
 public class Approach3 {
 
-
-        private static final Random random = new Random();
-
-        public static double getRandomNumber() {
-            return random.nextDouble();
-        }
-
         public static void main(String[] args) {
-            // Create multiple threads to generate random numbers concurrently
-            Thread thread1 = new Thread(() -> {
-                for (int i = 0; i <5; i++) {
-                    System.out.println("Thread 1 - Random Number: " + getRandomNumber());
-                }
-            });
+            // Generate random integers within a range using ThreadLocalRandom
+            // Generates random integer between 1 (inclusive) and 101 (exclusive)
+            int randomInt1 = ThreadLocalRandom.current().nextInt(1, 101);
+            System.out.println("Random Integer 1: " + randomInt1);
 
-            Thread thread2 = new Thread(() -> {
-                for (int i = 0; i < 5; i++) {
-                    System.out.println("Thread 2 - Random Number: " + getRandomNumber());
-                }
-            });
+            // Generate random doubles within a range using ThreadLocalRandom
+            // Generates random double between 0.0 (inclusive) and 1.0 (exclusive)
+            double randomDouble1 = ThreadLocalRandom.current().nextDouble(0.0, 1.0);
+            System.out.println("Random Double 1: " + randomDouble1);
 
-            // Start the threads
-            thread1.start();
-            thread2.start();
+            // Generate random integers within a range with a specified origin
+            // Generates random integer between 10 (inclusive) and 21 (exclusive)
+            int randomInt2 = ThreadLocalRandom.current().nextInt(10, 21);
+            System.out.println("Random Integer 2: " + randomInt2);
+
         }
-    }
+}
+
